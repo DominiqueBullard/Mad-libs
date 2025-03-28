26b495eb2f9df8e0721b1fe22d8e536c48161a3d
DominiqueBullard <dominique.miller21@gmail.com>
2025-03-28 18:48:17 +0000

    madlib

---
+++ madLibs.js
@@ -0,0 +1,10 @@
+const local= "abandon roller rink";
+const robot= "Rob";
+const name = "Bobert";
+let math= "8/5=1.6";
+
+const story =`In a hidden warehouse, ${name} stumbled upon a ${local}. The shiny machines were assembling gadgets endlessly. One robot, named ${robot}
+, handed ${name} a metallic keycard. It beeped and said, 'Access Granted!' Did you know ${math}"? Fascinating, right?`
+
+document.getElementById("madlibs-output").textContent = story;
+console.log(story)
